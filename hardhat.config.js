d246602f092f45aa1ae3f72e053c299bbc35278c
Jordan <jordanrob709@gmail.com>
2022-08-05 00:44:05 +0300

    added hardhat waffle & etherscan| modified hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 module.exports = {
@@ -8,10 +9,15 @@ module.exports = {
       chainId: 1337,
     },
     mumbai: {
-      url: process.env.STAGING_INFURA_URL,
+      url: process.env.STAGING_ALCHEMY_URL,
       accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
       gas: 2100000,
       gasPrice: 8000000000,
     },
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
 };
